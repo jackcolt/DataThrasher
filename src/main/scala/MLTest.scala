8a52d80f78d605a0b7af3f44c92d85c474eb390f
jackcolt <jack.colt@gmail.com>
2018-10-26 10:58:13 -0400

    added comments to MLTest

--- src/main/scala/MLTest.scala
+++ src/main/scala/MLTest.scala
@@ -4,6 +4,8 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.scalatest._
 
+//Tokenizer test for MLLib.
+
   class MLTest extends FunSuite {
 
     test ("mltest") {
